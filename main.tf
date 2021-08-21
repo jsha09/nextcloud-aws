b747ab65847626c43355de2d8134ae13be13e8ce
jsha09 <jhalaseh@gmail.com>
2021-08-21 17:20:51 +1000

    Switched to use aws-vpc module; Added EFS creation

--- main.tf
+++ main.tf
@@ -1,12 +1,24 @@
-module "network" {
-  source = "./modules/vpc"
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
 
-  aws_region     = var.aws_region
-  vpc_cidr       = var.vpc_cidr
-  public_a_cidr  = var.public_a_cidr
-  public_b_cidr  = var.public_b_cidr
-  private_a_cidr = var.private_a_cidr
-  private_b_cidr = var.private_b_cidr
+  name = "nextcloud-vpc"
+  cidr = "10.0.0.0/16"
 
-  #instance_id = module.nextcloud-app.nextcloud_core_instance_id
+  azs             = ["${var.aws_region}a", "${var.aws_region}b", "${var.aws_region}c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  manage_default_route_table = true
+  default_route_table_tags   = { DefaultRouteTable = true }
+
+  tags = {
+    Name      = "Nextcloud VPC"
+    Terraform = "true"
+  }
+}
+
+module "storage" {
+  source = "./modules/storage"
+
+  vpc_private_subnets = module.vpc.private_subnets
 }
